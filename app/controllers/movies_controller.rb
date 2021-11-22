ccafb4f9d1e9ca9dd3dad38766879514c7ba979c
Kiel Arneson <kiel.arneson@gmail.com>
2021-11-22 14:38:25 -0600

    Added create action so movie gets added to the database every time it is searched so long as it does not already exist

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,44 @@
 class MoviesController < ApplicationController
+  def create
+    # Really understand how I have access to imdb_id here!
+    movie = Movie.find_by(imdb_id: params[:imdb_id])
+
+    if movie
+      render json: { message: "This movie already exists in the database. See 'Show movie' in console for specific movie information" }, status: :im_used
+    else
+      movie = Movie.new(
+        title: params[:title],
+        year: params[:year],
+        rated: params[:rated],
+        released: params[:released],
+        runtime: params[:runtime],
+        formatted_runtime: params[:formatted_runtime],
+        actors: params[:actors],
+        awards: params[:awards],
+        box_office: params[:box_office],
+        director: params[:director],
+        genre: params[:genre],
+        language: params[:language],
+        plot: params[:plot],
+        poster: params[:poster],
+        entertainment_type: params[:type],
+        writer: params[:writer],
+        imdb_id: params[:imdb_id],
+        imdb_rating: params[:imdb_rating],
+        imdb_votes: params[:imdb_votes],
+        internet_movie_database_rating: params[:internet_movie_database_rating],
+        rotten_tomatoes_rating: params[:rotten_tomatoes_rating],
+        metacritic_rating: params[:metacritic_rating],
+      )
+
+      if movie.save
+        render json: { message: "This is a new movie for the database! Movie successfully saved", movie: movie }, status: :created
+      else
+        render json: { errors: movie.errors.full_messages }, status: :bad_request
+      end
+    end
+  end
+
   def show
     response = HTTP.get("http://www.omdbapi.com/?apikey=#{Rails.application.credentials.omdb_api_key}&t=#{params[:title]}")
     movie = JSON.parse(response.body)
